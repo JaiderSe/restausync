6e5ddbbb4da6a88850a94608aff60d8f5b5f04ea
Sebastián Quintero <154089423+JaiderSe@users.noreply.github.com>
2025-07-05 15:56:44 -0500

    vistas

--- trimestre 4/Proyecto/app/controllers/baker_controller.py
+++
@@ -1,9 +0,0 @@
-# app/controllers/baker_controller.py
-from flask import Blueprint, render_template
-
-baker_bp = Blueprint('baker_bp', __name__)
-
-@baker_bp.route('/baker/dashboard')
-def baker_dashboard():
-    return render_template('baker_dashboard.html')
-
--- trimestre 4/Proyecto/app/controllers/seller_controller.py
+++
@@ -1,9 +0,0 @@
-# app/controllers/seller_controller.py
-from flask import Blueprint, render_template
-
-seller_bp = Blueprint('seller_bp', __name__)
-
-@seller_bp.route('/seller/dashboard')
-def seller_dashboard():
-    return render_template('seller_dashboard.html')
-
